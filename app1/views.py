f3034bf31dc9ea96497f5818dca518bf7a303332
Krati Gupta <radhikaguptakrati@gmail.com>
2022-02-22 13:28:37 +0530

    Db connection and operation

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db import connection
 
 
 # Create your views here.
@@ -8,7 +9,19 @@ def xyz(request):
 
 
 def signup(request):
-    i=10
-    i=i+20
-    i=i+30
-    return render(request, "index.html")
+    email = request.GET['email']
+    password = request.GET['psw']
+    cursor = connection.cursor()
+    # to insert the data into database
+    query= "insert into users(email,password) values (%s,%s)"
+    value= (email, password)
+    cursor.execute(query, value)
+    print(cursor.rowcount)
+
+    #to fetch the data from database
+    #query = "select * from city where name='"+email+"'"
+    #cursor.execute(query)
+    #row = cursor.fetchone()
+    #print(row)
+    data = {"email": email, "password": password}
+    return render(request, "first.html", data)
--- pythonprojectfeb/settings.py
+++ pythonprojectfeb/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pythonprojectfeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PythonProjectFeb',
+        'USER': 'root',
+        'PASSWORD': 'krati@123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
